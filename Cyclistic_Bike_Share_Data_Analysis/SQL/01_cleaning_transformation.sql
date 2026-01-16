b1e6d2bd0d8e573614ac7a1e81644df9c5bbd010
Rosine Armelle Tinbo KAFANDO <armellekafando16@gmail.com>
2026-01-16 20:22:26 +0100

    Add data cleaning and transformation SQL script
    This script cleans and transforms Cyclistic bike-share data by calculating trip distance, ride length, day of the week, and start hour.

--- Cyclistic_Bike_Share_Data_Analysis/SQL/01_cleaning_transformation.sql
+++ Cyclistic_Bike_Share_Data_Analysis/SQL/01_cleaning_transformation.sql
@@ -1,1 +1,52 @@
+/*
+Project: Cyclistic Bike-Share Analysis
+Author: Rosine Armelle Tinbo KAFANDO
+GitHub: https://github.com/RosineKaf
+Description: Nettoyage et transformation des données Cyclistic
+Tools: SQL (BigQuery)
+*/
+
+-- =========================================================
+-- SECTION 1 : Calcul de la distance des trajets (en km)
+-- Utilisation des coordonnées GPS
+-- =========================================================
+CREATE OR REPLACE TABLE `cyclisticproject-451118.cyclic_data.all_trips_cleaned` AS
+SELECT
+    *,
+    ST_DISTANCE(
+        ST_GEOGPOINT(start_lng, start_lat),
+        ST_GEOGPOINT(end_lng, end_lat)
+    ) / 1000 AS trip_distance_km
+FROM `cyclisticproject-451118.cyclic_data.all_trips_cleaned`;
+
+
+-- =========================================================
+-- SECTION 2 : Calcul de la durée des trajets (en minutes)
+-- =========================================================
+CREATE OR REPLACE TABLE `cyclisticproject-451118.cyclic_data.all_trips_cleaned` AS
+SELECT
+    *,
+    TIMESTAMP_DIFF(ended_at, started_at, SECOND) / 60 AS ride_length_min
+FROM `cyclisticproject-451118.cyclic_data.all_trips_cleaned`;
+
+
+-- =========================================================
+-- SECTION 3 : Ajout du jour de la semaine
+-- 1 = Dimanche | 7 = Samedi
+-- =========================================================
+CREATE OR REPLACE TABLE `cyclisticproject-451118.cyclic_data.all_trips_cleaned` AS
+SELECT
+    *,
+    EXTRACT(DAYOFWEEK FROM started_at) AS day_of_week
+FROM `cyclisticproject-451118.cyclic_data.all_trips_cleaned`;
+
+
+-- =========================================================
+-- SECTION 4 : Ajout de l’heure de départ
+-- =========================================================
+CREATE OR REPLACE TABLE `cyclisticproject-451118.cyclic_data.all_trips_cleaned` AS
+SELECT
+    *,
+    EXTRACT(HOUR FROM started_at) AS start_hour
+FROM `cyclisticproject-451118.cyclic_data.all_trips_cleaned`;
 
